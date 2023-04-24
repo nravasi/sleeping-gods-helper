13af1beb75ebdaf0d9040618642a91d74989902c
Rodrigo Martin <rodrigoleonardomartin@gmail.com>
2023-04-24 15:10:56 -0300

    Reuse relationship code for other details (only the order changes), make everything look better

--- android/app/src/main/java/com/example/sgHelper/ui/details/details.kt
+++ android/app/src/main/java/com/example/sgHelper/ui/details/details.kt
@@ -1,28 +1,30 @@
 package com.example.sgHelper.ui.details
 
-import android.widget.Space
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -37,7 +39,6 @@ import com.example.sgHelper.model.RelationshipType
 import com.example.sgHelper.repository.FakeRepository
 import com.example.sgHelper.repository.Repository
 import com.example.sgHelper.ui.buildDetailsScreenRoute
-import com.fasterxml.jackson.annotation.ObjectIdGenerators.StringIdGenerator
 
 // Not sure if an activity should go here, I'm following a not-so-great tutorial I think
 
@@ -103,8 +104,12 @@ fun DetailsContent(
 
         for (relationship in entity.relationships) {
             RelationshipProperty(
-                navController = navController,
                 relationship = relationship,
+                onTap = {
+                    val route = buildDetailsScreenRoute(it.entity.id, it.entity.type)
+
+                    navController.navigate(route)
+                }
             )
             Spacer(modifier = Modifier.height(8.dp))
         }
@@ -119,27 +124,77 @@ fun DetailsContent(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RelationshipProperty(
-    navController: NavController,
-    relationship: EntityRelationship,
+fun DetailCard(
+    onClick: () -> Unit = {},
+    content: @Composable ColumnScope.() -> Unit
 ) {
-    val relationshipText = when (relationship.type) {
+    Surface(
+        shape = RoundedCornerShape(16.dp),
+        color = MaterialTheme.colorScheme.background,
+        modifier = Modifier
+            .height(IntrinsicSize.Min)
+            .padding(10.dp),
+        shadowElevation = 10.dp,
+        onClick = onClick,
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(2F),
+                verticalArrangement = Arrangement.Center,
+                content = content,
+            )
+        }
+    }
+}
+
+@Composable
+fun DetailTitle(
+    text: String
+) {
+    Text(
+        text = text,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colorScheme.onBackground
+    )
+}
+
+@Composable
+fun DetailSubtitle(
+    text: String
+) {
+    Text(
+        text = text,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Light,
+        color = MaterialTheme.colorScheme.onBackground
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RelationshipProperty(
+    relationship: EntityRelationship,
+    onTap: (EntityRelationship) -> Unit,
+) {
+    val subtitlePrefix = if (relationship.falopa) "Possibly " else ""
+    val subtitleRelationship = when (relationship.type) {
         RelationshipType.FOUND_AT -> "Found at"
         RelationshipType.REQUIRED_IN -> "Required in"
         RelationshipType.PROVIDES -> "Provides"
         RelationshipType.REQUIRES -> "Requires"
     }
 
-    val label = "$relationshipText ${relationship.entity.title}"
+    val subtitle = "$subtitlePrefix$subtitleRelationship"
 
-    Card(
-        onClick = {
-            val route = buildDetailsScreenRoute(relationship.entity.id, relationship.entity.type)
-
-            navController.navigate(route)
-        }
-    ) {
-        DetailsLabel(label = label)
+    DetailCard(onClick = { onTap(relationship)}) {
+        DetailSubtitle(text = subtitle)
+        DetailTitle(text = relationship.entity.title)
     }
 }
 
@@ -148,18 +203,22 @@ fun DetailsProperty(
     label: String,
     value: String
 ) {
-    Column(
-        modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
-    ) {
-        Divider()
-
-        DetailsLabel(label = label)
-        Text(
-            text = value,
-            modifier = Modifier.defaultMinSize(minHeight = 24.dp),
-            style = MaterialTheme.typography.bodyMedium
-        )
+    DetailCard {
+        DetailTitle(text = label)
+        DetailSubtitle(text = value)
     }
+//    Column(
+//        modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+//    ) {
+//        Divider()
+//
+//        DetailsLabel(label = label)
+//        Text(
+//            text = value,
+//            modifier = Modifier.defaultMinSize(minHeight = 24.dp),
+//            style = MaterialTheme.typography.bodyMedium
+//        )
+//    }
 }
 
 @Composable
